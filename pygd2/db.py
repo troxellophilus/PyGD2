ef25d2824c7c8c7df41f04ffc55d4c68bd6894c5
Drew Troxell <troxellus@gmail.com>
2016-02-03 23:39:18 -0800

    Pythonic rewrite of pygd2 and db

--- pygd2/db.py
+++ pygd2/db.py
@@ -1,9 +1,43 @@
-import sqlite3
+from peewee import SqliteDatabase, Model, BaseModel, TextField, DateTimeField
 
-def build() {
-    conn = sqlite3.connect('pygd2.db')
-    c = conn.cursor()
+
+database = SqliteDatabase('pygd2.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Player(BaseModel):
+    firstname = TextField()
+    lastname = TextField()
+    gdid = TextField(unique=True)
+    number = IntegerField(index=True)
+    boxname = TextField()
+    throws = CharField(index=True)
+    bats = CharField(index=True)
+    position = CharField(index=True)
+    status = CharField()
+    team = ForeignKeyField(Team, related_name='players')
+    date_modified = DateTimeField()
     
-    c.execute('DROP TABLE IF EXISTS players')
-    c.execute('CREATE TABLE IF NOT EXISTS players(name TEXT PRIMARY KEY, id INTEGER)')
-}
+    class Meta:
+        primary_key = CompositeKey('lastname', 'firstname')
+
+
+class Team(BaseModel):
+    gdid = TextField(unique=True)
+    abbrev = TextField(unique=True)
+
+
+def open():
+    database.connect()
+
+
+def close():
+    database.commit()
+    database.close()
+
+
+database.create_tables([Player], safe=True)
--- pygd2/pygd2.py
+++ pygd2/pygd2.py
@@ -16,131 +16,239 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+
 from datetime import datetime
-import sqlite3
+import logging
+import random
 import time
 import xml.etree.ElementTree as ET
 
 from bs4 import BeautifulSoup
 import requests
 
-class PyGd2(object):
-    def __init__(self):
-        self.GD_PREFIX = "http://gd2.mlb.com/components/game/mlb/"
-        self.GD_DATE_FMT = "year_{}/month_{:02d}/day_{:02d}/"
-        self.STAT_FMT = "http://m.mlb.com/lookup/json/named.sport_{}_composed.bam?game_type=%27R%27&league_list_id=%27mlb%27&sort_by=%27season_asc%27&player_id={}&sport_hitting_composed.season={}"
-
-        self.db = sqlite3.connect('pygd2.db')
-        self.db.row_factory = sqlite3.Row
-        self.cursor = self.db.cursor()
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS players(name TEXT PRIMARY KEY, id INTEGER)')
-
-    def update_players(self, date):
-        # update the database players, only run every so often
-        for pname, pid in self._get_players(date).items():
-            self._upsert_player(pname, pid)
-            time.sleep(2)
-        self.db.commit()
-
-    def _get_players(self, today):
-        players = {} # to be filled w/ player_name -> player_id mappings
-        # build the gd2 date dir url and request its source
-        gd_date = self.GD_DATE_FMT.format(today.year, today.month, today.day)
-        print(self.GD_PREFIX + gd_date)
-        response = requests.get(self.GD_PREFIX + gd_date)
-
-        # parse the gd2 date dir src for gid dirs, process players.xml for each
-        soup = BeautifulSoup(response.text, 'html.parser')
-        for link in soup.find_all('a'):
-            href = link.get('href')
-            print(href)
-            if href.startswith("gid_"):
-                xml = self.GD_PREFIX + gd_date + href + "players.xml"
-                print(xml)
-                players.update(self._process_players_xml(xml))
-            time.sleep(2)
-
-        # return as dictionary
-        return players
-
-    def _process_players_xml(self, url):
-        players = {} # to be filled w/ player_name -> player_id mappings
-        response = requests.get(url)
-        if response.status_code != requests.codes.ok:
-            return {}
-        root = ET.fromstring(response.content)
-        for player in root.iter('player'):
-            print(player.attrib)
-            name = "{} {}".format(player.attrib['first'], player.attrib['last'])
-            players[name] = player.attrib['id']
-        return players
-
-    def _upsert_player(self, pname, pid):
-        query = "INSERT OR IGNORE INTO players VALUES (?, ?)"
-        self.cursor.execute(query, (pname.lower(), pid))
-        query = "UPDATE players SET id=? WHERE name=?"
-        self.cursor.execute(query, (pid, pname.lower()))
-
-    def _process_data(self, data, year, stats):
-        buf = {}
-        res = {}
-        if isinstance(data, list):
-            for group in data:
-                if int(group['season']) == int(year):
-                    buf = group
-        elif isinstance(data, dict):
-            buf = data
-        else:
-            return res
-        for key, value in buf.items():
-            if key.lower() in stats:
-                res[key] = value
-        return res
-
-    def get_stats(self, player_name, year, stats):
-        result = {}
-        player_id = self._get_player_id(player_name.lower())
-        if not player_id:
-            print("Could not find player id.")
-            return result
-        raw_h, raw_p = self._get_player_stats(player_id, year)
-        data_h = raw_h['sport_hitting_composed']['sport_hitting_agg']['queryResults'] # TODO grab career and projected too
-        data_p = raw_p['sport_pitching_composed']['sport_pitching_agg']['queryResults']
-        stats = [x.lower() for x in stats]
-        if 'row' in data_h.keys():
-            season_h = self._process_data(data_h['row'], year, stats)
-        else:
-            season_h = {}
-        if 'row' in data_p.keys():
-            season_p = self._process_data(data_p['row'], year, stats)
-        else:
-            season_p = {}
-        return season_h, season_p
-
-    def _get_player_id(self, player_name):
-        query = "SELECT * FROM players WHERE name=?"
-        row = self.cursor.execute(query, (player_name,)).fetchone()
-        if row:
-            return row['id']
-        return False
-
-    def _get_player_stats(self, player_id, year):
-        response = requests.get(self.STAT_FMT.format("hitting", player_id, year))
-        if response.status_code != requests.codes.ok:
-            return
-        hitting = response.json()
-        response = requests.get(self.STAT_FMT.format("pitching", player_id, year))
-        if response.status_code != requests.codes.ok:
-            return
-        pitching = response.json()
-        return hitting, pitching
+import db
 
 
-def main():
-    gd = PyGd2()
+# Configure logger
+FORMAT = '%(levelname)s %(asctime)s %(module)s <%(lineno)d> %(message)s'
+logging.basicConfig(format=FORMAT)
+LOG = logging.getLogger('com.troxellophilus.pygd2')
 
-    print("Andre Ethier 2015: " + str(gd.get_stats('Andre Ethier', 2015, ["hr", "gidp", "avg", "slg"])))
-    print("Clayton Kershaw 2015: " + str(gd.get_stats('Clayton Kershaw', 2015, ["k", "whip", "avg", "era"])))
 
-if __name__ == '__main__':
-    main()
+# Define MLB URL formatting strings
+GD_URL_PRE = "http://gd2.mlb.com/components/game/mlb/"
+GD_DATE_FMT = "year_{}/month_{:02}/day_{:02d}/"
+
+M_URL_PRE = "http://m.mlb.com/lookup/json/"
+M_STAT_FMT = "named.sport_{}_composed.bam?player_id={}}&game_type=%27R%27&league_list_id=%27mlb%27&season={}"
+
+
+# Set randomized delay range in seconds
+DELAY_MIN = 0.5
+DELAY_MAX = 10
+
+
+def delay_fuzzy():
+    """Be kind."""
+    length = random.uniform(DELAY_MIN, DELAY_MAX)
+    time.sleep(length)
+
+
+def get_xml(url):
+    """Gets XML from a URL.
+    Args:
+        url: URL where the xml is.
+    Returns:
+        ElementTree of the XML file, or None
+    """
+    delay_fuzzy()
+    response = requests.get(url)
+    if response.status_code != requests.codes.ok:
+        LOG.error("Request to {}: status {}".format(url, response.status_code))
+        return None
+    return ET.fromstring(response.text)
+
+
+def get_json(url):
+    """Gets JSON from a url.
+    Args:
+        url: URL where the JSON is.
+    Returns:
+        json of the JSON response, or none.
+    """
+    delay_fuzzy()
+    response = requests.get(url)
+    if response.status_code != requests.codes.ok:
+        LOG.error("Request to {}: status {}".format(url, response.status_code))
+        return None
+    return response.json()
+
+
+def get_soup(url):
+    """Returns HTML soup from a url.
+    Args:
+        url: URL where the soup is.
+    Returns:
+        BeautifulSoup of the HTML file, or None
+    """
+    delay_fuzzy()
+    response = requests.get(url)
+    if response.status_code != requests.codes.ok:
+        LOG.error("Request to {}: status {}".format(url, response.status_code))
+        return None
+    return BeautifulSoup(response.text, 'html.parser')
+
+
+def get_players_xml_urls(date):
+    """Gets the URLs of the players.xml files for a given date.
+    Args:
+        date: Date to get players.xml files from (today if empty).
+    Returns:
+        List of players.xml urls.
+    """
+    if date is None:
+        date = datetime.today()
+    gd_date = GD_DATE_FMT.format(date.year, date.month, date.day)
+    soup = get_soup(GD_URL_PRE + gd_date)
+    if soup is None:
+        return []
+    players = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href.startswith("gid_"):
+            xml_url = GD_URL_PRE + gd_date + href + "players.xml"
+            players.append(xml_url)
+    return players
+
+
+def get_player_attribs(url):
+    """Gets a list of player attribute dicts from a players.xml file.
+    Args:
+        url: URL of a players.xml file.
+    Returns:
+        List of player attribute dicts.
+    """
+    xml = get_xml(url)
+    if xml is None:
+        return []
+    return [player.attrib for player in xml.iter('player')]:
+
+
+def update_gameday_ids(date = None):
+    """Updates the database players table w/ gameday ids from a given date.
+    Args:
+        date: Date to get gameday ids from (today if empty).
+    Returns:
+        List of the player objects updated in the database.
+    """
+    players_xml_urls = get_players_xml_urls(date)
+    db.open()
+    created = []
+    for url in players_xml_urls:
+        players = get_player_attribs(url)
+        for player in players:
+            for_team = db.Team.get_or_create(
+                id=player['team_id'],
+                abbrev=player['team_abbrev']
+            )
+            db_player, created = db.Player.get_or_create(
+                firstname=player['first'],
+                lastname=player['last'],
+                gdid=player['id'],
+                number=player['num'],
+                boxname=player['boxname'],
+                throws=player['rl'],
+                bats=player['bats'],
+                position=player['position'],
+                status=player['status'],
+                team=for_team,
+                date_modified=datetime.today()
+            )
+            if not created:
+                db_player.gdid = player['id']
+                db_player.date_modified = datetime.today()
+                db_player.save()
+            created.append(db_player)
+    db.close()
+    return created
+
+
+def get_player_stats(type='hitting', player_id='', year=datetime.today().year):
+    """Gets the player's stats for a year.
+    Args:
+        type: String type of stats (hitting or pitching)
+        player_id: Gameday id of the player.
+        year: Season to get stats from
+    Returns:
+        Dictionary of form {"stat_abbrev":"value"} with the stats.
+    """
+    url = M_URL_PRE + M_STAT_FMT.format(type, player_id, year)
+    json = get_json(url)
+    if json == None:
+        return {}
+    idx_composed = 'sport_{}_composed'.format(type)
+    idx_agg = 'sport_{}_agg'.format(type)
+    data = json[idx_composed][idx_agg]['queryResults']
+    if data['totalSize'] == 1: # TODO multi year in one request
+        return data['row']
+    return {}
+
+
+def get_player_by_name(first, last):
+    """Gets a player from the database.
+    Args:
+        first: The player's first name.
+        last: The player's last name (Scott Van Slyke -> last="Van Slyke")
+    Returns:
+        The Player, or None if player isn't in the database.
+    """
+    try:
+        player = db.Player.get(firstname=first, lastname=last)
+    except db.Player.DoesNotExist:
+        LOG.error("Player not in database.")
+        return None
+    else:
+        return player
+
+
+def get_pitching_stats_by_name(first, last, year, stats=[]): # TODO default stats
+    """Gets a player's pitching stats by name.
+    Args:
+        first: First name of the player.
+        last: Last name of the player (Scott Van Slyke -> last="Van Slyke")
+        year: Season to get stats from.
+        stats: List of stat abbreviations.
+    Returns:
+        Dictionary of form {"stat_abbrev":"value"}
+    """
+    player = get_player_by_name(first, last)
+    if player is None:
+        return {}
+    player_stats = get_player_stats('pitching', player.gdid, year)
+    stats_lower = [s.lower() for s in stats]
+    found_stats = {}
+    for abbrev, value in player_stats.items():
+        if stat_abbrev.lower() in stats_lower:
+            found_stats[abbrev] = value
+    return found_stats
+
+
+def get_batting_stats_by_name(first, last, year, stats=[]): # TODO default stats
+    """Gets a player's hitting stats by name.
+    Args:
+        player_id: Gameday id of the player.
+        year: Season to get stats from.
+        stats: List of stat abbreviations.
+    Returns:
+        Dictionary of form {"stat_abbrev":"value"}
+    """
+    player = get_player_by_name(first, last)
+    if player is None:
+        return {}
+    player_stats = get_player_stats('hitting', player.gdid, year)
+    stats_lower = [s.lower() for s in stats]
+    found_stats = {}
+    for abbrev, value in player_stats.items():
+        if stat_abbrev.lower() in stats_lower:
+            found_stats[abbrev] = value
+    return found_stats

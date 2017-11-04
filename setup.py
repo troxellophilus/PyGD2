29d2bad4bb81aa7e155c57871bcb64e33deead8c
Drew Troxell <troxellus@gmail.com>
2017-11-04 11:56:36 -0700

    setup license

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author = "Drew Troxell",
     author_email = "troxellus@gmail.com",
     description = ("Simple library for querying MLB's GD2 data."),
-    license = "GPLv3",
+    license = "MIT",
     keywords = "mlb gameday stats library",
     url = "http://github.com/DrewTroxell/PyGD2",
     install_requires=[
